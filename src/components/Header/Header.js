8e66483013ee5119a8ecab3690e8f6f24165a8f9
erikacalu <201810099@aluno.fjn.edu.br>
2019-03-20 15:42:01 -0300

    Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,9 @@ import './Header.css';
 // This Header will appears in all pages.
 const Header = () => (
 	<header id="main-header">
+		<ul id="logo">
+			<h4 id="stalebu">Stalebu</h4>
+		</ul>
 		<ul>
 			<li>Inicio</li>
 			<li>Contato</li>
